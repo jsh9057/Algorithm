38e05235687a27777f8c29f23a37250600fbf231
DESKTOP-IHMRBMU\Jeong <jsh9057@skuniv.ac.kr>
2020-07-10 14:33:43 +0900

    Dynamic Tile finish

--- src/Dynamic/NExpression.java
+++ src/Dynamic/NExpression.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class NExpression {
     public static void main(String args[]){
         int N=5;
-        int number = 127;
+        int number = 57;
         int sol = solution(N,number);
         System.out.println(sol);
     }
