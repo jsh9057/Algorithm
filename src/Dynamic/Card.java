fb4d33dd76737c015ab3c8730e5ab2e24776ab09
DESKTOP-IHMRBMU\Jeong <jsh9057@skuniv.ac.kr>
2020-07-17 15:21:26 +0900

    Dynamic Card finish

--- src/Dynamic/Card.java
+++ src/Dynamic/Card.java
@@ -1,7 +1,33 @@
 package Dynamic;
 
 public class Card {
+
+    static int[][] dp;
+    static boolean[][] visit;
+
     public static void main(String args[]){
-        
+        int[] left = {1,8,9,1,1,1,1};
+        int[] right= {8,9,3,3,3,3,3};
+        dp = new int[left.length][right.length];
+        visit = new boolean[left.length][right.length];
+        int answer = recur(0,0,left,right);
+        System.out.println(answer );
+    }
+
+    static int recur(int l, int r, int[] left, int[] right){
+        if(l == left.length || r== right.length) {
+            return 0;
+        }
+        if(visit[l][r]==true){
+            return dp[l][r];
+        }
+        dp[l][r] = Math.max(recur(l+1,r,left,right),recur(l+1,r+1,left,right));
+        if(left[l] > right[r]){
+            dp[l][r] = Math.max(dp[l][r],recur(l,r+1,left,right)+right[r]);
+        }
+
+        visit[l][r]=true;
+
+        return dp[l][r];
     }
 }
