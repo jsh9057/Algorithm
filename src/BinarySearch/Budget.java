cdf2de656a452d3131b361bd06e362764131648f
DESKTOP-IHMRBMU\Jeong <jsh9057@skuniv.ac.kr>
2020-07-29 16:09:24 +0900

    DFS/BFS BinarySearch Budget finish(update)

--- src/BinarySearch/Budget.java
+++ src/BinarySearch/Budget.java
@@ -8,28 +8,31 @@ public class Budget {
         int M =62;
 
         int answer=0;
+        answer=binarySearch(budgets,M);
+        System.out.println(answer);
+    }
+    public static int binarySearch(int[] budgets , int M){
+        int answer=0;
         int bLength=budgets.length;
         Arrays.sort(budgets);
         int left=M/bLength; //135
         int right=budgets[bLength-1]; //150
         long maxSum=0;
-        if(left>=right){ System.out.println(right);}
+        if(left>=right){ return right;}
         while (left<=right){
-        int mid=(left+right)/2; //
-        long sum=0;
+            int mid=(left+right)/2; //
+            long sum=0;
             for(int n: budgets){
                 if(n<=mid) {sum+=n;}
                 else {sum+=mid;}
             }
-            System.out.println("sum:"+sum);
-            System.out.println("left:"+left+" mid:"+mid+" right:"+right);
             if(sum>M){ right=mid-1;}
             else{
                 if(maxSum<sum){maxSum=sum; answer=mid;}
                 left=mid+1;
             }
         }
-        System.out.println(answer);
+        return answer;
     }
 
 }
