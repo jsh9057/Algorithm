766a238ee4a12e3062069bdbcf315e8275c5f3fd
DESKTOP-IHMRBMU\Jeong <jsh9057@skuniv.ac.kr>
2020-08-05 14:25:06 +0900

    BinarySearch SteppingStones finish

--- src/BinarySearch/Immigration.java
+++ src/BinarySearch/Immigration.java
@@ -11,25 +11,25 @@ public class Immigration {
         System.out.println(binarySearch(n,times));
     }
 
-    public static long binarySearch(int n, int[] times){
-        Arrays.sort(times);
-        int timeLength=times.length;
-        long left = 1;
-        long right = (long)times[timeLength-1]*n;
-        long mid=right;
-        long answer=right;
-        while(left<=right){
-            mid =(left+right)/2;
-            long count=0;
-            System.out.println("left: "+left+" mid: "+mid+" right: "+right);
-            for(int i=0; i<timeLength; i++){
-                count+=mid/times[i];
-            }
-            if(count<n){left=mid+1;}
-            else {
-                if(mid<answer) {answer = mid;}
-            right=mid-1;}
+public static long binarySearch(int n, int[] times){
+    Arrays.sort(times);
+    int timeLength=times.length;
+    long left = 1;
+    long right = (long)times[timeLength-1]*n;
+    long mid=right;
+    long answer=right;
+    while(left<=right){
+        mid =(left+right)/2;
+        long count=0;
+//            System.out.println("left: "+left+" mid: "+mid+" right: "+right);
+        for(int i=0; i<timeLength; i++){
+            count+=mid/times[i];
         }
-        return answer;
+        if(count<n){left=mid+1;}
+        else {
+            if(mid<answer) {answer = mid;}
+        right=mid-1;}
     }
+    return answer;
+}
 }
---
+++ src/BinarySearch/SteppingStones.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class SteppingStones {
+
+    public static void main(String args[]){
+        int distance=25;
+        int[] rocks={2,14,11,21,17};
+        int n=2;
+        SteppingStones steppingStones=new SteppingStones();
+        System.out.println(steppingStones.BinarySearch(distance,rocks,n));
+
+    }
+
+    public int BinarySearch(int distance, int[] rocks, int n){
+        int answer=0;
+        Arrays.sort(rocks);
+        int left=0;
+        int right=distance;
+        int mid=0;
+        while (left<=right){
+            mid=(left+right)/2;
+            int prv=0;
+            int removeCnt=0;
+            for(int i=0; i<rocks.length; i++){
+                if(rocks[i]-prv < mid){ removeCnt++; if(removeCnt>n){break;}}
+                else{ prv=rocks[i]; }
+            }
+            if(removeCnt>n){right=mid-1;}
+            else{answer=answer>mid? answer:mid; left=mid+1;}
+        }
+        return answer;
+    }
+}
+
+
